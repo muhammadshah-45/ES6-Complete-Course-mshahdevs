c243539d1a6a31485d7b87034b364871480a2c06
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-06-29 22:51:36 +0500

    I add new work

--- 07_map_ES6/map.js
+++ 07_map_ES6/map.js
@@ -1,6 +1,6 @@
 //Let's Start with map() method
 
-let numbers =[2,4,5,6,7];
+let numbers = [2, 4, 5, 6, 7];
 
 // let doubleNumbers = [];
 
@@ -8,20 +8,52 @@ let numbers =[2,4,5,6,7];
 //     doubleNumbers.push(numbers[i]*2);
 // }
 
-let doubleNumbers=numbers.map((number)=>{
-     number *2;
-    });
+// let doubleNumbers=numbers.map((number)=>{
+//      number *2;
+//     });
 
-console.log(doubleNumbers);
+// console.log(doubleNumbers);
 
 let cars = [
-    {model:"Civic",price:"Cheap"},
-    {model:"toyota",price:"expensive"}
+  { model: "Civic", price: "Cheap" },
+  { model: "toyota", price: "expensive" },
+];
+let prices = cars.map((car) => {
+  return car.model;
+});
+console.log(prices);
+
+const kvArray = [
+  { key: 1, name: "Shah" },
+  { key: 2, name: "Muhammad" },
+  { key: 3, name: "devs" },
 ];
 
-let prices = cars.map((car)=>{
-    return  car.model ;
+const reformattedArray = kvArray.map(({ key, name }) => ({ [key]: name }));
 
+// const reformattedArrays = kvArray.map(({ key, value }) => {
+//   // console.log([key="name"]);
+//   // console.log(value="umair");
+//   return { [key]: value };
+// });
+// console.log(reformattedArrays);
+console.log(reformattedArray); // [{ 1: 10 }, { 2: 20 }, { 3: 30 }]
+console.log("line41");
+console.log(kvArray);
+// [
+//   { key: 1, value: 10 },
+//   { key: 2, value: 20 },
+//   { key: 3, value: 30 }
+// ]
+
+let mobilePhones = [
+  { model: 2021, company: "samsung" },
+  { model: 2014, company: "apple" },
+  { model: 2022, company: "htc" },
+];
+
+const mobiles = mobilePhones.map(function (mobileModel) {
+  return mobileModel.company.length >= 5 ;
 });
-
-console.log(prices);
+console.log(mobilePhones);
+console.log(mobiles);

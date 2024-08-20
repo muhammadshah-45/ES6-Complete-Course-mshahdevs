2c9623a2577206b769f87be8663c6d7d955df2c3
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-08-20 22:27:04 +0500

    updated topics

--- 14_Destructuring/moreonWhentoUse.js
+++ 14_Destructuring/moreonWhentoUse.js
@@ -1,1 +1,18 @@
 console.log("more on ");
+
+const points = [
+    [3,2],
+    [4,8],
+    [5,7]
+];
+
+let result =points.map(([x,y]) =>{
+    // const [x,y] = pair;
+    // console.log("x:",pair[0]);
+    // console.log("y:",pair[1]);
+    // console.log(pair);
+    // return {firstPair:x,secondPair:y};
+    return [x,y];
+});
+
+console.log(result);
